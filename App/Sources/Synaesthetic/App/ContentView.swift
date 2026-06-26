4336d406cb6400f25c35c3578957a77831e6ba81
Nathan Landis <nathanlandis@gmail.com>
2026-06-26 09:30:15 -0400

    Multiple emitters

--- App/Sources/Synaesthetic/App/ContentView.swift
+++ App/Sources/Synaesthetic/App/ContentView.swift
@@ -11,6 +11,29 @@ struct ContentView: View {
                 color: .red,
                 highlightColor: .orange
             )
+
+            VStack {
+                HStack {
+                    Spacer()
+                    Emitter(
+                        radiusPercent: 0.2,
+                        color: .blue,
+                        highlightColor: .green
+                    )
+                    .frame(width: 200, height: 200)
+                }
+                Spacer()
+                HStack {
+                    Emitter(
+                        radiusPercent: 0.27,
+                        color: .purple,
+                        highlightColor: .red
+                    )
+                    .frame(width: 250, height: 250)
+                    Spacer()
+                }
+            }
+            .padding()
         }
     }
 }
